37eac75cf681427b0dc68aedc07dd5ee92b584c5
Dinesh <dineshv52@yahoo.in>
2022-09-19 23:42:29 +0530

    contact form

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function SendMail() {
     var params = {
         from_name : document.getElementById("Fullname").value,
         email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value
+        message : document.getElementById("message").value,
+        number : document.getElementById("number").value
     }
-    emailjs.send("service_i8dh3eg","template_67v7x5d", params).then(function (res) {
-        alert('Success',res.status);
-      });
+    emailjs.send("service_i8dh3eg","template_67v7x5d", params).then(function(response) 
+    {
+        console.log('SUCCESS!', response.status, response.text);
+     }, function(error) {
+        console.log('FAILED...', error);
+     });
 
 }
